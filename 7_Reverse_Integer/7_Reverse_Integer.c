32a6e1e76251eff47f00ebfcc687df2f0a6c5cd2
CYHsu <cy_hsu@cyberon.com.tw>
2020-08-21 14:10:17 +0800

    update 7_Reverse_Integer.c

--- 7_Reverse_Integer/7_Reverse_Integer.c
+++ 7_Reverse_Integer/7_Reverse_Integer.c
@@ -1,21 +1,16 @@
-// TODO: return 0 if result will overflow
 int reverse(int x){
     int result = 0;
     int stack[32] = {0};
     int idx = 0;
-    bool neg = false;
-    if(x < 0){
-        neg = true;
-        x *= -1;
-    }
-    while(x >= 1){
+    while(x != 0){
         stack[idx++] = x % 10;
         x /= 10;
     }
     for(int i=0; i<idx; i++){
+        if(result > INT_MAX/10 || (result == INT_MAX/10 && stack[i] > 7) ||
+           result < INT_MIN/10 || (result == INT_MIN/10  && stack[i] < -8))
+            return 0;
         result = result * 10 + stack[i];
     }
-    if(neg)
-        result *= -1;
     return result;
 }
