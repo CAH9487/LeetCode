cd21c4d5761fc16a13c8d6eced9ff2b4b23b7af5
CYHsu <cy_hsu@cyberon.com.tw>
2020-10-21 16:46:30 +0800

    update 36_Valid_Sudoku.py

--- 036_Valid_Sudoku/36_Valid_Sudoku.py
+++ 036_Valid_Sudoku/36_Valid_Sudoku.py
@@ -2,7 +2,10 @@ import unittest
 from typing import Any, List
 from collections import Counter
 
-
+'''
+Runtime: 108 ms, faster than 36.61% of Python3 online submissions
+Memory Usage: 14 MB, less than 100.00% of Python3 online submissions
+'''
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         for row in board:
@@ -11,8 +14,18 @@ class Solution:
         for col in list(map(list, zip(*board))):
             if(not self.check_array(col)):
                 return False
-
+        for subidx in range(9):
+            if(not self.check_array(self.get_subboard(board, subidx))):
+                return False
         return True
+
+    @staticmethod
+    def get_subboard(board: List[List[str]], idx: int) -> List[str]:
+        b = []
+        for col in range((idx//3)*3, (idx//3+1)*3):
+            for row in range((idx % 3)*3, (idx % 3+1)*3):
+                b.append(board[col][row])
+        return b
 
     @staticmethod
     def check_array(arr: List[str]) -> bool:
@@ -63,3 +76,4 @@ if __name__ == '__main__':
     unittest.main()
     # tc = TestCase()
     # tc.test_1()
+    # Solution.get_subboard([[]], 4)
